9901ef1f44a0669b5d1492e50e81c75aeab7ee24
Fernandoa-Reis <fernandoa.reis@outlook.com.br>
2025-06-13 14:01:21 +0000

    Criado as variaveis para carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,13 +2,22 @@
 
 int main() {
 //Variaveis Carta1
-char estado1;
-char codigo1[4];
-char cidade1[50];
-int populacao1;
-float area1;
-float pib1;
-int pontos_turisticos1;
+ char estado1;
+ char codigo1[4];
+ char cidade1[50];
+ int populacao1;
+ float area1;
+ float pib1;
+ int pontos_turisticos1;
+
+//Variaveis Carta2
+ char estado2;
+ char codigo2[4];
+ char cidade2[50];
+ int populacao2;
+ float area2;
+ float pib2;
+ int pontos_turisticos2;
 
 
 
